d5ee94cb7869c66981bf2956638cc93a8f9fb431
Script47 <54595497+Script47ph@users.noreply.github.com>
2023-12-31 01:33:48 +0700

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,17 +3,71 @@ package com.mycompany.app;
 /**
  * Hello world!
  */
+// public class App {
+
+//     private static final String MESSAGE = "Hello World!";
+
+//     public App() {}
+
+//     public static void main(String[] args) {
+//         System.out.println(MESSAGE);
+//     }
+
+//     public String getMessage() {
+//         return MESSAGE;
+//     }
+// }
+
+
+// Based on BalusC example from:
+// http://stackoverflow.com/questions/3732109/simple-http-server-in-java-using-only-java-se-api
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.Map;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
 public class App {
+    private static Map<String, String> env;
 
-    private static final String MESSAGE = "Hello World!";
-
-    public App() {}
-
-    public static void main(String[] args) {
-        System.out.println(MESSAGE);
+    public static void main(String[] args) throws Exception {
+	env = System.getenv();
+	int port = Integer.parseInt(System.getProperty("app.port"));
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
     }
 
-    public String getMessage() {
-        return MESSAGE;
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+	    // Logging IP address of request to stdout
+	    System.out.println("Request received from: " + t.getRemoteAddress().toString());
+
+	    // Displaying Hello message
+	    String hello = "Konnichiwa !!";
+	    String lang = env.get("HELLO_LANG");
+	    if (lang != null && lang.equals("JP")) {
+		hello = "こんにちは！";
+	    }
+            String response = "<html><body><h1>" + hello + "</h1>\n";
+
+	    // Displaying environment variables
+	    response += "<h2>Environment variables:</h2><ul>\n";
+	    for (String envName : env.keySet()) {
+		response += "<li>" + envName + '=' + env.get(envName) + "</li>\n";
+	    }
+	    response += "</ul></body></html>\n";
+
+	    // Sending response
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
     }
 }
